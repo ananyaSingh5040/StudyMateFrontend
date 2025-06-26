61956f40e744145bbea484b86c4edc65fb9b0731
Ananya Singh <singhananya5040@gmail.com>
2025-06-26 09:16:07 +0530

    26-06-25

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <ul className="nav-links">
+        <li>Dashboard</li>
+        <li>Planner</li>
+        <li>Notes</li>
+        <li>Doubt Solver</li>
+        <li>Profile</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
