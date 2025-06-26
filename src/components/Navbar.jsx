de74730de6be4d954d802b9d45fe8f9328fe2277
Ananya Singh <singhananya5040@gmail.com>
2025-06-26 09:15:58 +0530

    26-06-25

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="brand">StudyMate</div>
+      <div className="nav-actions">
+        <button className="nav-btn">Test Analysis</button>
+        <button className="nav-btn">Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
