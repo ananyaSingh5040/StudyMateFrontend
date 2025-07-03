ea8b376400fc9022990745cfd9b2b603a3e70322
Ananya Singh <singhananya5040@gmail.com>
2025-07-03 09:48:16 +0530

    profile

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,13 +4,12 @@ import BannerSlider from '../components/BannerSlider'
 function Dashboard() {
   return (
     <div className="dashboard">
-
-      <div className="rectangle slideshow">   <BannerSlider /></div>
-
+      <div className="rectangle slideshow"><BannerSlider /></div>
       <div className="square-section">
         <a href="/notes" className="card square-link">Your Notes</a>
         <a href="/planner" className="card square-link">Your Schedule</a>
       </div>
+
     </div>
   )
 }
