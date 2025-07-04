35281bbc05326d39fc16701b1097846135e3c2de
Ananya Singh <singhananya5040@gmail.com>
2025-07-04 09:05:25 +0530

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './Auth.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Handle login logic later
+    alert("Logged in with dummy data 🔐");
+  };
+
+  return (
+    <div className="auth-page">
+    <div className="auth-container">
+      <h2>Login to StudyMate</h2>
+      <form className="auth-form" onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        <p>
+          Don’t have an account? <a href="/signup">Sign up</a>
+        </p>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+export default Login;
