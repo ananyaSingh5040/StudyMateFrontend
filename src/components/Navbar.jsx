577bf408747df8ed3397eda8fae5b50df09de943
Ananya Singh <singhananya5040@gmail.com>
2025-07-16 09:12:41 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import { useNavigate } from 'react-router-dom';
 import './Navbar.css'
+import { toast } from "react-toastify";
 
 function Navbar() {
   const navigate = useNavigate();
+
+  const handleLogout = () => {
+  
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+
+    toast.info("Logged out successfully!");
+
+   
+    navigate("/login");
+  };
   return (
     <div className="navbar">
       <div className="brand">StudyMate</div>
       <div className="nav-actions">
         <button className="nav-btn" onClick={() => navigate('/profile')}>Profile</button>
-        <button className="nav-btn" onClick={() => navigate('/')}>Logout</button>
+        <button className="nav-btn" onClick={handleLogout}>Logout</button>
       </div>
     </div>
   )
